beede705b82a93b4cea0adce0a15d88219c4d863
Anil70530 <anilkumar62991@gmail.com>
2022-06-17 13:30:07 +0530

    again host refresh page

---
+++ src/js/navigation/navigation.js
@@ -0,0 +1,28 @@
+
+ 
+    onclickHandler = () => {
+      var addClassHeader = document.getElementsByClassName("aem-header");
+      addClassHeader[0].classList.add("aem-response");
+      var getElement = document.getElementById("navigation");
+      getElement.classList.add("navbar");
+      var getElementsClass = document.getElementsByClassName("aem-Offerbanner");
+      getElementsClass[0].classList.add("aem-isActive");
+      var getElementsHeaderClose = document.getElementsByClassName("aem-close");
+      getElementsHeaderClose[0].classList.add("aem-close-isActive");
+    };
+    closeHandler = () => {
+      var addClassHeader = document.getElementsByClassName("aem-header");
+      addClassHeader[0].classList.remove("aem-response");
+      var getElement = document.getElementById("navigation");
+      getElement.classList.remove("navbar");
+      var getElementsClass = document.getElementsByClassName("aem-Offerbanner");
+      getElementsClass[0].classList.remove("aem-isActive");
+      var getElementsHeaderClose = document.getElementsByClassName("aem-close");
+      getElementsHeaderClose[0].classList.remove("aem-close-isActive");
+    };
+  
+   
+
+  
+  
+  
