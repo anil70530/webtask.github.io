2afa27da888054b773d1d13e15fd3d8ff0a2cebe
Anil70530 <anilkumar62991@gmail.com>
2022-06-17 15:46:38 +0530

    update css issue fixed

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -32,7 +32,7 @@
     const findScreenSize = () => {
         let addClass = document.getElementById("compatible-device");
         if (className !== "") addClass.classList.remove(className);
-        var width = screen.width;
+        var width = window.innerWidth;
         console.log(width)
         if (width <= 320) className = "mobile";
         else if (width <= 991 && width >= 320) className = "tablet";
